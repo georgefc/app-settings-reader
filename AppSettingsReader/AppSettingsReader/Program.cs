74fc895c66446c8a6c5143b63d091e2c4865f837
George Fernando Camilo <george@georgefernando.com.br>
2021-03-08 00:39:20 -0300

    First commit

---
+++ AppSettingsReader/AppSettingsReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppSettingUtil;
+
+namespace AppSettingsReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value: {0}", AppSettingUtil.AppSettingUtil.GetValues<int>("IntValue"));
+            Console.WriteLine("Value: {0}", AppSettingUtil.AppSettingUtil.GetValues<bool>("BoolValue"));
+            Console.WriteLine("Value: {0}", AppSettingUtil.AppSettingUtil.GetValues<string>("StringValue"));
+            Console.ReadKey();
+        }
+    }
+}
